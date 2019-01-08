914047a90e7ca515a7fc74fbcebdb1aef74a179c
Jorge Suárez Porras <Jorge@MacBook-Air-de-Jorge.local>
2019-01-08 13:40:43 +0100

    Ultima Version

---
+++ App-Swoosh/App-Swoosh/Controller/CategoriasVC.swift
@@ -0,0 +1,22 @@
+
+//
+//  ProductoVC.swift
+//  App-Swoosh
+//
+//  Created by Jorge Suárez Porras on 08/01/2019.
+//  Copyright © 2019 Jorge Suárez Porras. All rights reserved.
+//
+
+import UIKit
+
+class CategoriasVC: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ App-Swoosh/App-Swoosh/Controller/PendientesVC.swift
@@ -0,0 +1,21 @@
+//
+//  PendientesVC.swift
+//  App-Swoosh
+//
+//  Created by Jorge Suárez Porras on 08/01/2019.
+//  Copyright © 2019 Jorge Suárez Porras. All rights reserved.
+//
+
+import UIKit
+
+class PendientesVC: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
